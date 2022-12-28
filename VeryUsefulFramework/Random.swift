9d37b7ed2ac4b1b793a5097820456786d693bf21
Oğuzhan Karakuş <35741818+oguzhankarakus@users.noreply.github.com>
2022-12-28 23:09:59 +0300

    update random method

--- VeryUsefulFramework/Random.swift
+++ VeryUsefulFramework/Random.swift
@@ -9,11 +9,11 @@ import Foundation
 
 public final class Random {
     
-    public func getRandomNumbers(in range: Range<Int>) -> Int {
+    public func getRandomNumbers(in range: ClosedRange<Int>) -> Int {
         randomNumbers(in: range)
     }
     
-    private func randomNumbers(in range: Range<Int>) -> Int {
+    private func randomNumbers(in range: ClosedRange<Int>) -> Int {
         Int.random(in: range)
     }
     
